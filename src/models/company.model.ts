d05598ad95b3e35b947513a48dd9659afc218287
bishnu thapa <bishnuthapa1820@gmail.com>
2024-07-22 22:42:19 +0545

    feat: company model done only

---
+++ src/models/company.model.ts
@@ -0,0 +1,105 @@
+import mongoose, { Schema, Document, Model, Types } from "mongoose";
+import type { TUser } from "./user.model";
+import type { TCategory } from "./category.modal";
+
+const { String, Boolean, ObjectId } = Schema.Types;
+
+export type TCompany = Document & {
+  name: string;
+  opening_time: string;
+  closing_time: string;
+  description: string;
+  contact_number: string;
+  is_deleted: boolean;
+  cover_image: {
+    url: string;
+    fileId: string;
+    fileName: string;
+    contentType: string;
+  } | null;
+  location: { latitude: string; longitude: string };
+  category: Types.ObjectId | TCategory | null;
+  created_by: Types.ObjectId | TUser | null;
+  updated_date: Date | null;
+  created_date: Date;
+};
+
+const companySchema = new Schema<TCompany>({
+  name: {
+    type: String,
+    required: [true, "Company name is required."],
+  },
+  opening_time: {
+    type: String,
+    required: [true, "Opening hour is required."],
+  },
+
+  closing_time: {
+    type: String,
+    required: [true, "Closing hour is required."],
+  },
+
+  description: {
+    type: String,
+    required: [true, "Description is required."],
+  },
+
+  contact_number: {
+    type: String,
+    required: [true, "Contact number is required."],
+  },
+
+  cover_image: {
+    type: {
+      url: String,
+      fileId: String,
+      fileName: String,
+      contentType: String,
+    },
+    default: null,
+  },
+
+  is_deleted: {
+    type: Boolean,
+    default: false,
+  },
+
+  location: {
+    latitude: String,
+    longitude: String,
+  },
+
+  created_by: {
+    type: ObjectId,
+    ref: "User",
+    default: null,
+    required: [true, "Created by is required."],
+  },
+
+  category: {
+    type: ObjectId,
+    ref: "Category",
+    default: null,
+    required: [true, "Category is required."],
+  },
+
+  updated_date: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+
+  created_date: {
+    type: Date,
+    required: true,
+    immutable: true,
+    default: Date.now,
+  },
+});
+
+const Company: Model<TCompany> = mongoose.model<TCompany>(
+  "Company",
+  companySchema
+);
+
+export default Company;
